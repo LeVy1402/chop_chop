e4bcee3842161f0de09ac3f21415dc9a169cc34c
LeVy1402 <leevy1402@gmail.com>
2026-05-20 23:11:20 +0700

    Add local data source for categories feature

---
+++ lib/categories/data/data_source/categories_local_data_source.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../core/error/exceptions.dart';
+import '../models/category_model.dart';
+
+abstract class CategoriesLocalDataSource {
+  /// Get the cached [List<CategoryModel>] which was gotten the last time
+  /// the user had an internet connection.
+  ///
+  /// Throws [CacheException] if no cached data is present.
+  Future<List<CategoryModel>> getLastCategories();
+
+  Future<void> cacheCategories(List<CategoryModel> categories);
+}
+
+class CategoriesLocalDataSourceImpl implements CategoriesLocalDataSource {
+  static const cachedCategoriesKey = 'CACHED_CATEGORIES';
+
+  final SharedPreferences sharedPreferences;
+
+  CategoriesLocalDataSourceImpl({required this.sharedPreferences});
+
+  @override
+  Future<void> cacheCategories(List<CategoryModel> categories) async {
+    await sharedPreferences.setString(
+      cachedCategoriesKey,
+      json.encode(categories.map((e) => e.toJson()).toList()),
+    );
+  }
+
+  @override
+  Future<List<CategoryModel>> getLastCategories() async {
+    final jsonString = sharedPreferences.getString(cachedCategoriesKey);
+    if (jsonString == null) {
+      throw CacheException();
+    }
+    final decoded = json.decode(jsonString) as List<dynamic>;
+
+    return decoded
+        .map((e) => CategoryModel.fromJson(e as Map<String, dynamic>))
+        .toList();
+  }
+}
---
+++ test/categories/data/data_source/categories_local_data_source_test.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+
+import 'package:chop_chop/categories/data/data_source/categories_local_data_source.dart';
+import 'package:chop_chop/categories/data/models/category_model.dart';
+import 'package:chop_chop/core/error/exceptions.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../../../fixtures/fixture_reader.dart';
+import 'categories_local_data_source_test.mocks.dart';
+
+@GenerateMocks([SharedPreferences])
+void main() {
+  late CategoriesLocalDataSourceImpl dataSource;
+  late MockSharedPreferences mockSharedPreferences;
+
+  setUp(() {
+    mockSharedPreferences = MockSharedPreferences();
+    dataSource = CategoriesLocalDataSourceImpl(
+      sharedPreferences: mockSharedPreferences,
+    );
+  });
+
+  group('getCategories', () {
+    test(
+      'should return CategoryListModel from SharedPreferences when there is one in the cache',
+      () async {
+        final tCategoryModels =
+            (json.decode(fixture('categories_cached.json')) as List)
+                .map((e) => CategoryModel.fromJson(e as Map<String, dynamic>))
+                .toList();
+        // arrange
+        when(
+          mockSharedPreferences.getString(any),
+        ).thenReturn(fixture('categories_cached.json'));
+        // act
+        final result = await dataSource.getLastCategories();
+        // assert
+        verify(mockSharedPreferences.getString('CACHED_CATEGORIES'));
+        expect(result, equals(tCategoryModels));
+      },
+    );
+
+    test('should throw a CacheException where there is not cache value', () {
+      when(mockSharedPreferences.getString(any)).thenReturn(null);
+      final call = dataSource.getLastCategories;
+      expect(() => call(), throwsA(isA<CacheException>()));
+    });
+  });
+
+  group('cacheCategories', () {
+    final tCategoriesModel = [
+      CategoryModel(
+        name: 'Appetizers',
+        imageUrl: 'https://images.unsplash.com/photo-1544025162-d76694265947',
+      ),
+    ];
+
+    test('should call SharePreference to cache the data', () async {
+      when(
+        mockSharedPreferences.setString(any, any),
+      ).thenAnswer((_) async => true);
+      dataSource.cacheCategories(tCategoriesModel);
+      verify(
+        mockSharedPreferences.setString(
+          'CACHED_CATEGORIES',
+          json.encode(tCategoriesModel),
+        ),
+      );
+    });
+  });
+}
